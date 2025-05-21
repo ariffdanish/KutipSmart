580cedec97f1593d639c1e35768fa6edeb817f71
ariffdanish <muhammadariffdanish@graduate.utm.my>
2025-05-21 22:59:34 +0800

    Update Truck.cs

--- KutipSmart/Models/Truck.cs
+++ KutipSmart/Models/Truck.cs
@@ -1,11 +1,18 @@
-﻿namespace KutipSmart.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KutipSmart.Models
 {
     public class Truck
     {
+        [Key]
         public int TruckId { get; set; }
+        [Display(Name = "Truck No")]
         public string TruckNo { get; set; }
+        [Display(Name = "Driver")]
         public string DriverName { get; set; }
+        [Display(Name = "Phone")]
         public string Phone { get; set; }
+        [Display(Name = "Company")]
         public string Company { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.Now;
         public DateTime UpdatedAt { get; set; } = DateTime.Now;
