591a5e1d918e2b733dc8dd547564c92eb5791162
ariffdanish <muhammadariffdanish@graduate.utm.my>
2025-05-21 22:55:42 +0800

    model truck

---
+++ KutipSmart/Models/Truck.cs
@@ -0,0 +1,14 @@
+﻿namespace KutipSmart.Models
+{
+    public class Truck
+    {
+        public int TruckId { get; set; }
+        public string TruckNo { get; set; }
+        public string DriverName { get; set; }
+        public string Phone { get; set; }
+        public string Company { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+
+    }
+}
