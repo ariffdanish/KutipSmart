86ebed1f69e878d3a79179fb88cc667cde94a5ba
harriz <danial.harriz360@gmail.com>
2025-05-23 00:01:17 +0800

    Bin

--- KutipSmart/Models/Bin.cs
+++ KutipSmart/Models/Bin.cs
@@ -8,23 +8,29 @@ namespace KutipSmart.Models
         [Key]
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "Bin Number is required")]
         [Display(Name = "Bin")]
         public string BinNo { get; set; }
 
+        [Required(ErrorMessage = "Street is required")]
         [Display(Name = "Street")]
-        public string Street {  get; set; }
+        public string Street { get; set; }
 
+        [Required(ErrorMessage = "City is required")]
         [Display(Name = "City")]
         public string City { get; set; }
 
+        [Required(ErrorMessage = "State is required")]
         [Display(Name = "State")]
         public string State { get; set; }
 
+        [Required(ErrorMessage = "PostCode is required")]
         [Display(Name = "PostCode")]
-        public string PostCode {  get; set; }
+        public string PostCode { get; set; }
 
         [Display(Name = "Created")]
         public DateTime CreatedAt { get; set; } = DateTime.Now;
+
         public DateTime UpdatedAt { get; set; } = DateTime.Now;
     }
 }
