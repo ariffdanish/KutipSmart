098350f2d184d10cb2b6e12e404161dbd4d91b73
ariffdanish <muhammadariffdanish@graduate.utm.my>
2025-05-25 22:20:10 +0800

    upda

---
+++ KutipSmart/Migrations/20250525135954_DinUpdLagiii.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KutipSmart.Migrations
+{
+    /// <inheritdoc />
+    public partial class DinUpdLagiii : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "TruckNo",
+                table: "Trucks",
+                type: "nvarchar(13)",
+                maxLength: 13,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(10)",
+                oldMaxLength: 10);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "TruckNo",
+                table: "Trucks",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(13)",
+                oldMaxLength: 13);
+        }
+    }
+}
--- KutipSmart/Models/Truck.cs
+++ KutipSmart/Models/Truck.cs
@@ -9,26 +9,36 @@ namespace KutipSmart.Models
     {
         [Key]
         public int TruckId { get; set; }
+
         [Display(Name = "Truck No")]
         [Required]
         [StringValidator(InvalidCharacters = "~!@#$%^&*()_+`-={}|[]:;\"'<>,.?/\\")]
-        [StringLength(10)]
+        [Remote(action: "IsTruckNoAvailable", controller: "Trucks", AdditionalFields = "TruckId", ErrorMessage = "Truck number already exists.")]
+        [RegularExpression(@"^[A-Z0-9]+$", ErrorMessage = "Truck number must be alphanumeric.")]
+        [StringLength(13)]
         public string TruckNo { get; set; }
+
         [Display(Name = "Status")]
         public TruckStatus Status { get; set; } = TruckStatus.Active;
+
         [Display(Name = "Driver")]
         [Required]
+        [RegularExpression(@"^[A-Za-z\s]+$", ErrorMessage = "Driver name must contain only letters and spaces.")]
         [StringLength(100)]
         public string DriverName { get; set; }
+
         [Display(Name = "Phone")]
         [Required]
         [StringLength(15)]
         [RegularExpression(@"^\+?[0-9]*$", ErrorMessage = "Phone number must be numeric.")]
         public string Phone { get; set; }
+
         [Display(Name = "Company")]
         [Required]
         [StringLength(100)]
         public string Company { get; set; }
+
+
         [Display(Name ="Created")]
         public DateTimeOffset CreatedAt { get; set; } = DateTime.Now;
         public DateTimeOffset UpdatedAt { get; set; } = DateTime.Now;
