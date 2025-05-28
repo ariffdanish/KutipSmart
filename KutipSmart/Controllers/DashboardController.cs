bcef0a26901f958c32c939b24d27dd18afc616ab
Che Marhumi <128233086+CheMrhumi@users.noreply.github.com>
2025-05-28 14:41:40 +0800

    kambing

--- KutipSmart/Controllers/DashboardController.cs
+++ KutipSmart/Controllers/DashboardController.cs
@@ -3,6 +3,7 @@ using KutipSmart.Models;
 using System.Linq;
 using System.Collections.Generic;
 using KutipSmart.Data;
+using System;
 
 namespace KutipSmart.Controllers
 {
@@ -10,26 +11,60 @@ namespace KutipSmart.Controllers
     {
         private readonly KutipDbContext _context;
 
-        // Constructor to inject the DB context
         public DashboardController(KutipDbContext context)
         {
             _context = context;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(
+            string cityFilter,
+            string stateFilter,
+            BinStatus? binStatusFilter,
+            string truckNoFilter,
+            TruckStatus? truckStatusFilter
+        )
         {
-            // Fetch real data from the database
-            var bins = _context.Bin.ToList();
-            var trucks = _context.Trucks.ToList();
+            var binsQuery = _context.Bin.AsQueryable();
+            var trucksQuery = _context.Trucks.AsQueryable();
 
-            var model = new DashboardViewModel
+            // Bin filters
+            if (!string.IsNullOrEmpty(cityFilter))
+                binsQuery = binsQuery.Where(b => b.City == cityFilter);
+
+            if (!string.IsNullOrEmpty(stateFilter))
+                binsQuery = binsQuery.Where(b => b.State == stateFilter);
+
+            if (binStatusFilter.HasValue)
+                binsQuery = binsQuery.Where(b => b.Status == binStatusFilter.Value);
+
+            // Truck filters
+            if (!string.IsNullOrEmpty(truckNoFilter))
+                trucksQuery = trucksQuery.Where(t => t.TruckNo.Contains(truckNoFilter));
+
+            if (truckStatusFilter.HasValue)
+                trucksQuery = trucksQuery.Where(t => t.Status == truckStatusFilter.Value);
+
+            var viewModel = new DashboardViewModel
             {
-                Bins = bins,
-                Trucks = trucks
+                Bins = binsQuery.ToList(),
+                Trucks = trucksQuery.ToList()
             };
 
-            return View(model);
+            // Dropdown data
+            ViewBag.AllCities = _context.Bin.Select(b => b.City).Distinct().ToList();
+            ViewBag.AllStates = _context.Bin.Select(b => b.State).Distinct().ToList();
+            ViewBag.AllBinStatuses = Enum.GetValues(typeof(BinStatus)).Cast<BinStatus>().ToList();
+            ViewBag.AllTruckStatuses = Enum.GetValues(typeof(TruckStatus)).Cast<TruckStatus>().ToList();
+            ViewBag.AllTruckNos = _context.Trucks.Select(t => t.TruckNo).Distinct().ToList();
+
+            // Selected filters
+            ViewBag.SelectedCity = cityFilter;
+            ViewBag.SelectedState = stateFilter;
+            ViewBag.SelectedBinStatus = binStatusFilter;
+            ViewBag.SelectedTruckNo = truckNoFilter;
+            ViewBag.SelectedTruckStatus = truckStatusFilter;
+
+            return View(viewModel);
         }
     }
 }
-
